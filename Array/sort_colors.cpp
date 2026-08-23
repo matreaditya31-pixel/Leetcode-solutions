ca95dc30fdbd14cbee203b055b326da73f76960b
matreaditya31-pixel <matreaditya31@gmail.com>
2026-08-23 18:17:50 +0530

    Add sort colors solution

---
+++ Array/sort_colors.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int hi = nums.size()-1;
+        int lo=0;
+        int mid=0;
+        while(mid<=hi){
+            if(nums[mid]==2){
+                int temp = nums[mid];
+                nums[mid]=nums[hi];
+                nums[hi]=temp;
+                hi--;
+            }
+            else if(nums[mid]==0){
+                 int temp = nums[mid];
+                nums[mid]=nums[lo];
+                nums[lo]=temp;
+                lo++;
+                mid++;
+            }
+            else mid++;
+        }
+    }
+};
