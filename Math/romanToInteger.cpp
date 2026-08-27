9d6284f61a5c369973af70162cda472f42aebf09
matreaditya31-pixel <matreaditya31@gmail.com>
2026-08-27 23:54:36 +0530

    Roman to integer Solution

---
+++ Math/romanToInteger.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int ans = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int curr;
+
+            // Convert Roman symbol to number
+            if (s[i] == 'I') curr = 1;
+            else if (s[i] == 'V') curr = 5;
+            else if (s[i] == 'X') curr = 10;
+            else if (s[i] == 'L') curr = 50;
+            else if (s[i] == 'C') curr = 100;
+            else if (s[i] == 'D') curr = 500;
+            else curr = 1000;   // M
+
+            // If current is smaller than next, subtract
+            if (i + 1 < s.length()) {
+                int next;
+
+                if (s[i + 1] == 'I') next = 1;
+                else if (s[i + 1] == 'V') next = 5;
+                else if (s[i + 1] == 'X') next = 10;
+                else if (s[i + 1] == 'L') next = 50;
+                else if (s[i + 1] == 'C') next = 100;
+                else if (s[i + 1] == 'D') next = 500;
+                else next = 1000;
+
+                if (curr < next)
+                    ans -= curr;
+                else
+                    ans += curr;
+            }
+            else {
+                ans += curr;
+            }
+        }
+
+        return ans;
+    }
+};
