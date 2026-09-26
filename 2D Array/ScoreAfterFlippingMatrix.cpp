58b3940cec53061c61c165ed49d6fa5f6e19fbfd
matreaditya31-pixel <matreaditya31@gmail.com>
2026-09-26 15:07:29 +0530

    solution of Score after Flipping matrix

---
+++ 2D Array/ScoreAfterFlippingMatrix.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int matrixScore(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+          for(int i=0;i<rows;i++){
+            if(grid[i][0] == 0) {
+                for(int j=0;j<cols;j++){
+                    if(grid[i][j]==0)
+                        grid[i][j] =1;
+                        else
+                            grid[i][j] =0;
+
+                    }
+                }
+            }
+           // flip the column where noz>noo
+            for(int j=0;j<cols;j++){
+                 int noo=0;
+                 int noz=0;
+                 for(int i=0;i<rows;i++){
+                    if(grid[i][j] == 0) noz++;
+                    else noo++;
+                 }
+            
+            if(noz>noo){ //flip
+                 for(int i=0;i<rows;i++){
+                    if(grid[i][j] == 0) grid[i][j] = 1;
+                    else grid[i][j] = 0;
+                 }
+              }
+            }
+            int sum = 0;
+            for(int i=0;i<rows;i++){
+                int x=1;
+                for(int j=cols-1;j>=0;j--){
+                    sum= sum+grid[i][j] * x;
+                    x = x*2;
+                }
+            }
+        return sum;
+     }
+};
